50a6887648084b490272565f43c934382e6069f3
andreasholt <6665487+AndreasHolt@users.noreply.github.com>
2024-08-11 18:05:34 +0200

    feat: initial SignalR and AvaloniaUI setup

---
+++ MultilingualChat.Server/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace MultilingualChat.Server.Hubs;
+
+public class ChatHub: Hub
+{
+    // SendMessage can be called by a connected client to send a message to all other clients.
+    // 1. My guess is that the client connects to endpoint (/chat).
+    // 2. They can then invoke the exposed function SendMessage at that endpoint, which then sends the message to all other endpoints
+    public async Task SendMessage(string message, string user)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", message, user);
+        
+    }
+    
+    
+}
